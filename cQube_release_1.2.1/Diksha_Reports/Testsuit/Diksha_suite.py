fd502af6653a9cd7fedce28440229e31cb5a4a4f
chetanDG <chetan.goudar@tibilsolutions.com>
2020-08-27 15:55:54 +0530

    "Final commit"

--- cQube_release_1.2.1/Diksha_Reports/Testsuit/Diksha_suite.py
+++ cQube_release_1.2.1/Diksha_Reports/Testsuit/Diksha_suite.py
@@ -2,8 +2,9 @@ import unittest
 
 from HTMLTestRunner import HTMLTestRunner
 
-from Diksha_Reports.Diksha_charts import  diksha_chart_Regression_testing
+from Diksha_Reports.Diksha_charts import diksha_chart_Regression_testing, diksha_chart
 from Diksha_Reports.Diksha_column_chart import column_chart
+from Diksha_Reports.Diksha_table_report import diksha_table
 from get_dir import pwd
 
 
@@ -25,22 +26,38 @@ class MyTestSuite(unittest.TestCase):
         )
         runner1.run(smoke_test)
 
-    # def test_issue02(self):
-    #     smoke_test = unittest.TestSuite()
-    #     smoke_test.addTests([
-    #         unittest.defaultTestLoader.loadTestsFromTestCase(diksha_table.cQube_diskha_report),
-    #     ])
-    #     p = pwd()
-    #     outfile = open("/home/chetan/Desktop/cQubeTesting/Diksha_Reports/Testsuit/DikshaReports_regression.html", "a")
-    #
-    #     runner1 = HTMLTestRunner.HTMLTestRunner(
-    #         stream=outfile,
-    #         title='Diksha Table Regression Report',
-    #         verbosity=1,
-    #
-    #     )
-    #     runner1.run(smoke_test)
-    #     outfile.close()
+    def test_issue02(self):
+        smoke_test = unittest.TestSuite()
+        smoke_test.addTests([
+            # file name .class name
+            unittest.defaultTestLoader.loadTestsFromTestCase(diksha_chart.cQube_diskha_chart),
+        ])
+        p = pwd()
+        outfile = open("path",'w')
+        runner1 = HTMLTestRunner.HTMLTestRunner(
+            stream=outfile,
+            title='Diksha chart Test Report',
+            verbosity=1,
+
+        )
+        runner1.run(smoke_test)
+
+    def test_issue03(self):
+        smoke_test = unittest.TestSuite()
+        smoke_test.addTests([
+            unittest.defaultTestLoader.loadTestsFromTestCase(diksha_table.cQube_diskha_report),
+        ])
+        p = pwd()
+        outfile = open("Testsuit/DikshaReports_regression.html", "a")
+
+        runner1 = HTMLTestRunner.HTMLTestRunner(
+            stream=outfile,
+            title='Diksha Table Regression Report',
+            verbosity=1,
+
+        )
+        runner1.run(smoke_test)
+        outfile.close()
     @classmethod
     def tearDown(self):
         self.driver.close()
